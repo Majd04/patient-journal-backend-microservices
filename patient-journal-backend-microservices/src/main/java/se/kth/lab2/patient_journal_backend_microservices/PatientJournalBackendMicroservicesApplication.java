9b602b6cfa04fabff7273e036a598235730d4492
Majd Matloob <Majd.matloob04@gmail.com>
2025-11-08 23:56:49 +0100

    First commit: Initial project upload

---
+++ patient-journal-backend-microservices/src/main/java/se/kth/lab2/patient_journal_backend_microservices/PatientJournalBackendMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package se.kth.lab2.patient_journal_backend_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientJournalBackendMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientJournalBackendMicroservicesApplication.class, args);
+	}
+
+}
---
+++ patient-journal-backend-microservices/src/test/java/se/kth/lab2/patient_journal_backend_microservices/PatientJournalBackendMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.kth.lab2.patient_journal_backend_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientJournalBackendMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
